4a3b2a7bf50ea2e58935155d8fc50e6ad75950ce
dbe <davidbrianethier@gmail.com>
2019-09-12 19:23:17 -0400

    4,1

--- 4/1.py
+++ 4/1.py
@@ -7,12 +7,72 @@ def run(lines):
     events = [normalize_dates(line) for line in parsed_lines]
     grouped = group_by_date(events)
 
+    #Add sleep schedule to each date
     for date in grouped:
-        print(date)
-        print(grouped[date]['guard'])
+        sleep = sleep_by_min(grouped[date]['actions'])
+        grouped[date]['sleep'] = sleep
+
+    sleepiest = sleepiest_guard(grouped)
+    min = most_slept_min(sleepiest, grouped)
+
+    return sleepiest * min
 
 def parse(line):
     return re.findall(REGEX, line)[0]
+
+def parse_guard(action):
+    regex = r'Guard #(\d+) begins shift'
+    return int(re.findall(regex, action)[0])
+
+def most_slept_min(guard, grouped):
+    sleep_schedules = []
+    for date in grouped:
+        if(grouped[date]['guard'] == guard):
+            sleep_schedules.append(grouped[date]['sleep'])
+
+    sums = list(map(sum, zip(*sleep_schedules)))
+    
+    return sums.index(max(sums))
+
+def sleepiest_guard(grouped):
+    by_guard = sleep_count_by_guard(grouped)
+
+    most_sleep = -1
+    sleepy_guy = None
+
+    for guard in by_guard:
+        if(by_guard[guard] > most_sleep):
+            most_sleep = by_guard[guard]
+            sleepy_guy = guard
+
+    return sleepy_guy
+
+
+def sleep_count_by_guard(grouped):
+    by_guard = {}
+
+    for date in grouped:
+        guard = grouped[date]['guard']
+        sleep = by_guard.get(guard, 0)
+        sleep += sum(grouped[date]['sleep'])
+        by_guard[guard] = sleep
+
+    return by_guard
+
+def sleep_by_min(actions):
+    sleep = [0] * 60
+    sorted_actions = sorted(actions, key=lambda action: action[0])
+
+    #Take every 2 actions (paired up/down)
+    for i in range(0, int(len(sorted_actions) / 2)):
+        up = sorted_actions[i * 2][0]
+        down = sorted_actions[i * 2 + 1][0]
+
+        for j in range(up, down):
+            sleep[j] = 1
+
+    return sleep
+
 
 #Input: (min, date, action)
 # Output: {'3-11':
@@ -22,11 +82,6 @@ def parse(line):
 #                 (33, 'down')
 #              ]}
 #         }
-
-def parse_guard(action):
-    regex = r'Guard #(\d+) begins shift'
-    return int(re.findall(regex, action)[0])
-
 def group_by_date(events):
     by_date = {}
 
