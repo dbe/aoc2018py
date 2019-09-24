0ab81e26fb3ce91eaa240c3ebdd275bfdfc4dd06
dbe <davidbrianethier@gmail.com>
2019-09-23 23:36:18 -0400

    6,1

--- 6/1.py
+++ 6/1.py
@@ -4,38 +4,78 @@ from string import ascii_lowercase
 import re
 
 def run(lines):
-    # points = [parse(line) for line in lines]
-    # points = [(int(a),int(b)) for (a,b) in points]
-    points = [
-        (1,1),
-        (1,6),
-        (8,3),
-        (3,4),
-        (5,5),
-        (8,9)
-    ]
+    points = [parse(line) for line in lines]
+    points = [(int(a),int(b)) for (a,b) in points]
+    # points = [
+    #     (1,1),
+    #     (1,6),
+    #     (8,3),
+    #     (3,4),
+    #     (5,5),
+    #     (8,9)
+    # ]
     points = transpose(points)
     (_, maxx, _, maxy) = min_max(points)
-    map = [[None] * (maxy + 1)] * (maxx + 1)
 
+    #Build up map of points listing their closest Main Point
+    #The purpose is to find which points are on the outer edge in order to disqualify them
+    #Because they have infinite area
+    map = [ [None for y in range(maxy + 1)] for x in range(maxx + 1)]
     for y in range(maxy + 1):
         for x in range(maxx + 1):
             closest = inf
 
             for point in points:
                 dist = manhattan(point, (x, y))
-                print(f"Dist from {x}, {y} to point is {dist}")
                 if(dist < closest):
                     map[x][y] = points.index(point)
                     closest = dist
                 elif(dist == closest):
                     map[x][y] = '.'
 
-    pretty_print(map)
-    return "Oreo"
+    edge_indices = get_edge_indices(map)
+    valid_indices = set()
+    for i in range(len(points)):
+        if(i not in edge_indices):
+            valid_indices.add(i)
+
+    max_area = 0
+    for index in valid_indices:
+        area = count_area(map, index)
+        if(area > max_area):
+            max_area = area
+
+
+
+    return max_area
+
+def count_area(map, index):
+    count = 0
+
+    for col in map:
+        for e in col:
+            if(e == index):
+                count += 1
+
+    return count
+
+def get_edge_indices(map):
+    point_indices = set()
+
+    for y in range(0, len(map[0])):
+        for x in range(0, len(map)):
+            #Top or bottom row
+            if(y == 0 or y == len(map[0]) - 1):
+                if(map[x][y] != '.'):
+                    point_indices.add(map[x][y])
+            #First or last column
+            elif(x == 0 or x == len(map) - 1):
+                if(map[x][y] != '.'):
+                    point_indices.add(map[x][y])
+
+    return point_indices
 
 def pretty_print(map):
-    print(map)
     for y in range(0, len(map[0])):
         for x in range(0, len(map)):
             print(map[x][y], end='')
