3af2fa04ae49e1321b5b54edf890c45ce39b501b
dbe <davidbrianethier@gmail.com>
2019-09-25 15:03:20 -0400

    8,1

---
+++ 8/1.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def run(lines):
+    elements = map(int, lines[0].split(' '))
+    # elements = [2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2]
+    tree = deque(elements)
+
+    return parseNode(tree.popleft(), tree.popleft(), tree)
+
+def parseNode(children, metadata, tree):
+    total = 0
+
+    for i in range(children):
+        total += parseNode(tree.popleft(), tree.popleft(), tree)
+
+    for j in range(metadata):
+        total += tree.popleft()
+
+    return total
