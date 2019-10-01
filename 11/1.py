f2de0f7b10b48d8b4fc931af7f3cd7c62239ae55
dbe <davidbrianethier@gmail.com>
2019-10-01 17:28:10 -0400

    Adding in test cases for TDD

--- 11/1.py
+++ 11/1.py
@@ -1,8 +1,20 @@
 def run(lines):
-    # serial = int(lines[0])
-    serial = 18
+    serial = int(lines[0])
+
+    return "Oreo"
 
 
 
 def power(x, y, serial):
-    pass
+    return x
+
+def test_power():
+    assert(power(3,5,8) == 4)
+    assert(power(122,79,57) == -5)
+    assert(power(217,196,39) == 0)
+    assert(power(101,153,71) == 4)
+
+
+def test_run():
+    assert(run(['18']) == (33, 45))
+    assert(run(['42']) == (21, 61))
