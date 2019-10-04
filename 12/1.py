4134398c4ebf53b653d418e1cd2ac144e7dc80cb
dbe <davidbrianethier@gmail.com>
2019-10-03 23:38:31 -0400

    Cleaning up parsing using some clever techniques found online

--- 12/1.py
+++ 12/1.py
@@ -1,55 +1,31 @@
 from collections import defaultdict
-
 import re
 
-REGEX = r'(.{5}) => (.)'
-
 def run(lines):
-#     lines ='''initial state: #..#.#..##......###...###
-#
-# ...## => #
-# ..#.. => #
-# .#... => #
-# .#.#. => #
-# .#.## => #
-# .##.. => #
-# .#### => #
-# #.#.# => #
-# #.### => #
-# ##.#. => #
-# ##.## => #
-# ###.. => #
-# ###.# => #
-# ####. => #'''.split('\n')
+    #Just get all discreet sections of .'s and #'s
+    #The first one is the initial state, and all of the rest of the rules
+    initial, *pairs = re.findall(r'[.#]+', '\n'.join(lines))
 
-    state = lines[0][15:]
-    d = defaultdict(lambda: '.')
+    #Setup defaultdict mapping pot position to value. Anything not specified will be a '.'
+    pots = defaultdict(lambda: '.')
+    pots.update( {i: v for i, v in enumerate(initial)} )
 
-    print(f"state: {state}")
-
-    return 'oreo'
-
-    rules = [lines[i] for i in range(2, len(lines))]
-    rule_map = {}
-
-    for rule in rules:
-        (a, b) = re.findall(REGEX, rule)[0]
-        rule_map[a] = b
-
-    print(f"rule_map: {rule_map}")
-
-    print(''.join(state))
-    for i in range(100):
-        state = evolve(state, rule_map)
-        print(''.join(state))
+    #Clever use of extended ranges to zip even and odd indices from the pairs to combine them
+    rules = {i: v for i,v in zip(pairs[::2], pairs[1::2])}
 
 
-    total = 0
-    for i in range(len(state)):
-        if(state[i] == '#'):
-            total += i - padding
 
-    return total
+    # for i in range(100):
+    #     state = evolve(state, rule_map)
+    #     print(''.join(state))
+    #
+    #
+    # total = 0
+    # for i in range(len(state)):
+    #     if(state[i] == '#'):
+    #         total += i - padding
+    #
+    # return total
 
 
 def evolve(state, rule_map):
