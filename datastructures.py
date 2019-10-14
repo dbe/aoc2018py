eb249e03e0d9dbf5d8860bcffc889601c80f1376
dbe <davidbrianethier@gmail.com>
2019-10-14 01:09:56 -0400

    Finishing implementation of priority heap

--- datastructures.py
+++ datastructures.py
@@ -18,18 +18,41 @@ class PriorityQueue:
         return self.data[0].v if len(self.data) > 0 else None
 
     def pop(self):
-        pass
+        ret = self.data[0].v
+        self.data[0] = self.data[len(self.data) - 1]
+        self.data.pop()
+        self._bubble_down(0)
+        return ret
+
+    def __len__(self):
+        return len(self.data)
+
+    def _bubble_down(self, i):
+        #If not child
+        if(i * 2 + 1 < len(self.data)):
+            smaller = self._smaller_child(i)
+            self._swap(i, smaller)
+            self._bubble_down(smaller)
+
+    def _smaller_child(self, i):
+        if(i * 2 + 2 >= len(self.data)):
+            return i * 2 + 1
+        else:
+            if(self.data[i * 2 + 1].p < self.data[i * 2 + 2].p):
+                return i * 2 + 1
+            else:
+                return i * 2 + 2
+
 
     def _bubble_up(self, i):
-        while(i > 0):
+        if(i > 0):
             p = self._parent(i)
-
             if(self.data[i].p < self.data[p].p):
                 self._swap(i, p)
-                i = p
-            else:
-                return
+                self._bubble_up(p)
 
+    def _is_child_node(self, i):
+        pass
 
     def _parent(self, i):
         return int((i - 1) / 2)
@@ -61,3 +84,7 @@ if(__name__ == "__main__"):
     print(f"p.peek(): {p.peek()}")
     add_and_print(p, "a")
     print(f"p.peek(): {p.peek()}")
+
+    while(len(p) > 0):
+        print(f"p: {p}")
+        print(f"p.pop(): {p.pop()}")
